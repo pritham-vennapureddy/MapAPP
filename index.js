87dd19ed6904600c22d0298c13dcb44cd22fdbb3
Pritham Vennapureddy <prithamreddy46@gmail.com>
2025-03-24 18:43:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express=require("express")
 const jwt=require("jsonwebtoken")
 const cors=require("cors")
 const app=express()
-const bcrypt=require("'bcryptjs'")
+const bcrypt=require("bcryptjs")
 app.use(express.json())
 app.use(cors())
 const users = [
